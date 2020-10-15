282bb8ee8d175903ac3814b410fbeee15787ed51
PRANAV KUMAR SINGH <PRANAV KUMAR SINGH@LAPTOP-56AE49HV>
2020-10-15 15:45:10 +0530

    saved

---
+++ OnlineShopping/src/com/lti/model/Customers.java
@@ -0,0 +1,19 @@
+package com.lti.model;
+
+public class Customers {
+int Custid;
+String CustName;
+public int getCustid() {
+	return Custid;
+}
+public void setCustid(int custid) {
+	Custid = custid;
+}
+public String getCustName() {
+	return CustName;
+}
+public void setCustName(String custName) {
+	CustName = custName;
+}
+
+}
