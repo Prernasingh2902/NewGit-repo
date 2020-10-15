fea067c9dfbbfd2c954466017e7ff123f4b0a8e7
PRANAV KUMAR SINGH <PRANAV KUMAR SINGH@LAPTOP-56AE49HV>
2020-10-15 15:49:45 +0530

    added custcity

--- OnlineShopping/src/com/lti/model/Customers.java
+++ OnlineShopping/src/com/lti/model/Customers.java
@@ -3,6 +3,13 @@ package com.lti.model;
 public class Customers {
 int Custid;
 String CustName;
+String CustCity;
+public String getCustCity() {
+	return CustCity;
+}
+public void setCustCity(String custCity) {
+	CustCity = custCity;
+}
 public int getCustid() {
 	return Custid;
 }
