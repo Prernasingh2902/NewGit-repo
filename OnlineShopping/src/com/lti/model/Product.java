0283e2e278a64971efc56fc145b3415b2b239bcc
PRANAV KUMAR SINGH <PRANAV KUMAR SINGH@LAPTOP-56AE49HV>
2020-10-16 09:27:53 +0530

    added product to br1

---
+++ OnlineShopping/src/com/lti/model/Product.java
@@ -0,0 +1,19 @@
+package com.lti.model;
+
+public class Product {
+int proid;
+String proname;
+public int getProid() {
+	return proid;
+}
+public void setProid(int proid) {
+	this.proid = proid;
+}
+public String getProname() {
+	return proname;
+}
+public void setProname(String proname) {
+	this.proname = proname;
+}
+
+}
