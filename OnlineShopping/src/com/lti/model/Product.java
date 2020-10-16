ccf9e7dc015eeb5fc455fe4f35280a11f183ec04
Prernasingh2902 <72907620+Prernasingh2902@users.noreply.github.com>
2020-10-16 10:16:23 +0530

    Update Product.java

--- OnlineShopping/src/com/lti/model/Product.java
+++ OnlineShopping/src/com/lti/model/Product.java
@@ -2,12 +2,19 @@ package com.lti.model;
 
 public class Product {
 int proid;
+int qty;
 String proname;
 public int getProid() {
 	return proid;
 }
+public int qty() {
+	return qty;
+}
 public void setProid(int proid) {
 	this.proid = proid;
+}
+	public void setqty(int qty) {
+	this.qty = qty;
 }
 public String getProname() {
 	return proname;
