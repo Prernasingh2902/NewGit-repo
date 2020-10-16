2f32864b271afe0f5741e9bba03a14a7d9317618
PRANAV KUMAR SINGH <PRANAV KUMAR SINGH@LAPTOP-56AE49HV>
2020-10-16 09:45:58 +0530

    added account to br1

---
+++ OnlineShopping/src/com/lti/model/Account.java
@@ -0,0 +1,19 @@
+package com.lti.model;
+
+public class Account {
+int accId;
+int bal;
+public int getAccId() {
+	return accId;
+}
+public void setAccId(int accId) {
+	this.accId = accId;
+}
+public int getBal() {
+	return bal;
+}
+public void setBal(int bal) {
+	this.bal = bal;
+}
+
+}
