acc3c4dde439f2af0ae757f49bc9e237bdfef8e6
Prernasingh2902 <72907620+Prernasingh2902@users.noreply.github.com>
2020-10-16 10:24:07 +0530

    Update Product.java

--- OnlineShopping/src/com/lti/model/Product.java
+++ OnlineShopping/src/com/lti/model/Product.java
@@ -7,8 +7,14 @@ String proname;
 public int getProid() {
 	return proid;
 }
+	public int price() {
+	return price;
+}
 public void setProid(int proid) {
 	this.proid = proid;
+}
+public void price(int price) {
+	this.price = price;
 }
 public String getProname() {
 	return proname;
